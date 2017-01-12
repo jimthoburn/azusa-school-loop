f292ebca48a28da5b67edacc61fe00e4f02e09a5
Jim Thoburn <jim@jimthoburn.com>
2017-01-11 23:42:24 -0800

    Add more examples pages (generated by Node.js and Jekyll).

---
+++ get-pages.js
@@ -0,0 +1,30 @@
+
+var fs = require('fs');
+var jsdom = require("jsdom");
+
+var pages = [{url: '/', title: 'Home'}];
+
+jsdom.env(
+  "http://ahs-ausd-ca.schoolloop.com/portal/site_map?d=x&return_url=1484205684870",
+  function (err, window) {
+    var document = window.document;
+    var links = document.querySelectorAll('a');
+    var href;
+    for (var index = 0; index < links.length; index++) {
+      href = links[index].getAttribute('href');
+      if (href.indexOf('/') !== 0 || href.indexOf('/portal') === 0 || href.indexOf('/misc') === 0) {
+        console.log('skipped: ' + href);
+        continue;
+      }
+      pages.push({
+        url: href,
+        title: links[index].textContent
+      });
+    }
+    fs.writeFile('_data/pages.json', JSON.stringify(pages), 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+);
