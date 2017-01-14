dcf70baa57bc57d73d5872ab8a837ff8b1f686e5
Jim Thoburn <jim@jimthoburn.com>
2017-01-13 17:42:22 -0800

    Make secondary pages look nicer.

--- _node/make-pages-options.js
+++ _node/make-pages-options.js
@@ -19,6 +19,24 @@ exports.LOGO_HTML = `
 <p>Home of the Mighty Aztecs</p>
 `;
 
+exports.FOOTER_HTML = `
+<div class="contact">
+  <div>
+    <h2>Azusa High School</h2>
+    <p>240 North Cerritos Avenue<br />Azusa, CA 91702 </p>
+    <p>626-815-3400</p>
+  </div>
+
+  <p><strong>Linda McNary, Principal</strong><br /><a href="mailto:lmcnary@azusa.org">lmcnary@azusa.org</a></p>
+  <p><strong>Lorrie Brown, School Secretary</strong><br /><a href="mailto:lbrown@azusa.org">lbrown@azusa.org</a></p>
+</div>
+
+<div class="legal">
+  <h2><a href="http://azusa.org"><img src="/images/azusa-district.png" width="100" height="100" alt="Azusa Unified School District" /></a></h2>
+  <p>The District prohibits, at any district school or school activity, unlawful discrimination, harassment, intimidation, and bullying of any student based on the student’s actual race, color, ancestry, national origin, ethnic group, identication, age, religion, marital or parental status, physical or mental disability, sex, sexual orientation, gender, gender identity, or gender expression; the perception of one or more of such characteristics; or association with a person or group with one or more these actual or perceived characteristics.</p>
+</div>
+`;
+
 exports.SITE_MAP_URL = 'http://ahs-ausd-ca.schoolloop.com/portal/site_map?d=x';
 
 exports.SECTIONS_DATA = [{
